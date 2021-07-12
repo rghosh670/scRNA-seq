57b6590a9ee5c1b7562eb40184149707f102a8b7
Rohit Ghosh <rohitghosh101@gmail.com>
2021-07-12 15:40:03 -0700

    added linear fits

--- pca/hvg_heatmap.py
+++ pca/hvg_heatmap.py
@@ -7,7 +7,7 @@ import matplotlib
 norm = 'raw'
 
 df = pd.read_csv('pca/' + norm + '/' + norm + '_HVGs.csv', index_col=0)
-# df = df.drop(['pat-10', 'lev-11', 'act-4'])
+df = df.drop(['pat-10', 'lev-11', 'act-4', 'mlc-3'])
 # df['mean'] = df.mean(axis=1)
 # df = df.sort_values(by = 'mean')
 # df = df.drop('mean', axis = 1)
